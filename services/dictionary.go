0171208bc3d13ccf778210e9ae4ee3c89b8ee362
Maxim Zhiburt <zhiburt@gmail.com>
2019-02-09 20:33:25 +0300

    Add first service Dictionary

---
+++ services/dictionary.go
@@ -0,0 +1,56 @@
+package services
+
+import (
+	"context"
+	"time"
+
+	"github.com/dictionary/models/word"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/gofrs/uuid"
+)
+
+// Dictionary service discribes dictionary
+type Dictionary interface {
+	AddNewWord(ctx context.Context, w word.Word) (string, error)
+	GetByID(ctx context.Context, id string) (word.Word, error)
+}
+
+// dictionary implements the Dictionary Service
+type dictionary struct {
+	logger     log.Logger
+	repository word.Repository
+}
+
+// NewDictionary create and return new Dictionary
+func NewDictionary(rep word.Repository, logger log.Logger) Dictionary {
+	return &dictionary{
+		logger:     logger,
+		repository: rep,
+	}
+}
+
+func (d *dictionary) AddNewWord(ctx context.Context, w word.Word) (string, error) {
+	logger := log.With(d.logger, "method", "AddNewWord")
+	uuid, _ := uuid.NewV4()
+	id := uuid.String()
+	w.ID = id
+	w.Timestamp = time.Now().Unix()
+
+	if err := d.repository.AddWordInto(ctx, w); err != nil {
+		level.Error(logger).Log("error", err)
+		return "", word.ErrRepository
+	}
+	return id, nil
+}
+
+func (d *dictionary) GetByID(ctx context.Context, id string) (word.Word, error) {
+	logger := log.With(d.logger, "method", "GetByID")
+
+	w, err := d.repository.GetWordByID(ctx, id)
+	if err != nil {
+		level.Error(logger).Log("erorr", err)
+		return w, err
+	}
+	return w, nil
+}
