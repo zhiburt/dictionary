70ba08db3234aa051a608a03795eac020435aefd
Maxim Zhiburt <zhiburt@gmail.com>
2019-02-15 19:09:01 +0300

    Remove older folder

--- models/word/wrapbadger/wrapbadger.go
+++
@@ -1,9 +0,0 @@
-package wrapbadger
-
-import "github.com/dgraph-io/badger"
-
-// DB interface budger for mocking in tests
-type DB interface {
-	View(func(*badger.Txn) error) error
-	NewTransaction(bool) *badger.Txn
-}
